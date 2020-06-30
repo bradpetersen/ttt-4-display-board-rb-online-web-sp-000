c8a526f17539070cb65a6bd37194f29581fdd90d
Brad Petersen <brad.petersen7@gmail.com>
2020-06-30 21:01:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,18 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def tic_tack_toe_row
+  "   |   |   "
+end
+
+def tic_tack_toe_separator
+  "-----------"
+end
+
+def display_board
+  puts tic_tack_toe_row
+  puts tic_tack_toe_separator
+  puts tic_tack_toe_row
+  puts tic_tack_toe_separator
+  puts tic_tack_toe_row
+end
