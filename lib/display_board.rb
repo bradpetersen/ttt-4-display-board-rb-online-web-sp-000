f8277d917bd5c2cb4914331f7ec4eff5d387afba
Brad Petersen <brad.petersen7@gmail.com>
2020-06-30 21:35:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-puts board = [" "," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 
-def display_board
+
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -11,4 +12,4 @@ def display_board
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-output = capture_puts{display_board(board)}
+display_board(board)
